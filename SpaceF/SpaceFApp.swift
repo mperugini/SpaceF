6c95e3ff4b8a79784d01c895124deaa97e30fb23
Mariano Perugini <mariano.perugini@despegar.com>
2025-06-03 16:46:17 -0300

    Initial Commit

---
+++ SpaceF/SpaceFApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpaceFApp.swift
+//  SpaceF
+//
+//  Created by Mariano Peruginoi on 03/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct SpaceFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
